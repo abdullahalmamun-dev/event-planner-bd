2281fa6ffdca44f7771b845961406a5fa316052c
hummerheads <mdabdullah.allinfozone@gmail.com>
2024-09-07 02:35:46 +0600

    first commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className="text-9xl font-black	">This is Home</p>
+            
+        </div>
+        
+    );
+};
+
+export default Home;
